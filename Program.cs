bef9a6f023a77a4c1a4bad41ac2870d0c8a2049f
Greg Keogh <gfkeogh@gmail.com>
2022-09-09 11:43:31 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+using RCS.Carbon.Shared;
+using RCS.Carbon.Tables;
+
+namespace Carbon.Demo.Command
+{
+	internal class Program
+	{
+		const string TestLicensing = "https://rcsapps.azurewebsites.net/licensing2test/";
+
+		static async Task Main(string[] args)
+		{
+			var engine = new CrossTabEngine();
+			await engine.GetFreeLicence("Demo App", TestLicensing);
+			engine.OpenJob("client1rcs", "demo");
+			var sprops = new XSpecProperties();
+			var dprops = new XDisplayProperties();
+			dprops.Output.Format = XOutputFormat.TSV;
+			string report = engine.GenTab("Demo of Age × Region", "Age", "Region", null, null, sprops, dprops);
+			Console.WriteLine(report);
+			engine.CloseJob();
+			Console.WriteLine("PAUSE...");
+			Console.ReadLine();
+		}
+	}
+}
