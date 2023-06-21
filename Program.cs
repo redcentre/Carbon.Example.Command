d6c6d9dcd87a8bfe47ec867fa6f04cd18255ab53
Greg Keogh <gfkeogh@gmail.com>
2023-06-21 15:24:52 +1000

    Use live licensing address.

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Threading.Tasks;
+using RCS.Carbon.Licensing.RedCentre;
 using RCS.Carbon.Shared;
 using RCS.Carbon.Tables;
 
@@ -7,9 +8,14 @@ namespace Carbon.Example.Command
 {
 	internal class Program
 	{
+		// Choose either the live or testing licensing server base address.
+		// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+		const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2/";
+		//const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2test/";
+
 		static async Task Main(string[] args)
 		{
-			var engine = new CrossTabEngine();
+			var engine = new CrossTabEngine(new RedCentreLicensingProvider(LicenceBaseAddress));
 			await engine.GetFreeLicence("GitHub Example App");
 			engine.OpenJob("rcsruby", "demo");
 			var sprops = new XSpecProperties();
