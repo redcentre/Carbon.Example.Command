91f320f03dbdb706134c325dcae3076fad0c6070
Greg Keogh <gfkeogh@gmail.com>
2024-06-20 16:28:49 +1000

    8.8.12 Updated to Carbon 8.9.23. Target net80. Code cleanup.

--- Program.cs
+++ Program.cs
@@ -4,28 +4,27 @@ using RCS.Carbon.Licensing.RedCentre;
 using RCS.Carbon.Shared;
 using RCS.Carbon.Tables;
 
-namespace Carbon.Example.Command
-{
-	internal class Program
-	{
-		// Choose either the live or testing licensing server base address.
-		// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
-		const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2/";
-		//const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2test/";
+namespace Carbon.Example.Command;
 
-		static async Task Main(string[] args)
-		{
-			var engine = new CrossTabEngine(new RedCentreLicensingProvider(LicenceBaseAddress));
-			await engine.GetFreeLicence("GitHub Example App");
-			engine.OpenJob("rcsruby", "demo");
-			var sprops = new XSpecProperties();
-			var dprops = new XDisplayProperties();
-			dprops.Output.Format = XOutputFormat.TSV;
-			string report = engine.GenTab("Demo of Age × Region", "Age", "Region", null, null, sprops, dprops);
-			Console.WriteLine(report);
-			engine.CloseJob();
-			Console.WriteLine("PAUSE...");
-			Console.ReadLine();
-		}
+internal class Program
+{
+	// Choose either the live or testing licensing server base address.
+	// ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓
+	const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2/";
+	//const string LicenceBaseAddress = "https://rcsapps.azurewebsites.net/licensing2test/";
+
+	static async Task Main(string[] args)
+	{
+		var engine = new CrossTabEngine(new RedCentreLicensingProvider(LicenceBaseAddress));
+		await engine.GetFreeLicence("GitHub Example App");
+		engine.OpenJob("rcsruby", "demo");
+		var sprops = new XSpecProperties();
+		var dprops = new XDisplayProperties();
+		dprops.Output.Format = XOutputFormat.TSV;
+		string report = engine.GenTab("Demo of Age × Region", "Age", "Region", null, null, sprops, dprops);
+		Console.WriteLine(report);
+		engine.CloseJob();
+		Console.WriteLine("PAUSE...");
+		Console.ReadLine();
 	}
 }
